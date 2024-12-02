da3c5f620d26a842a70115f5dd7bfba4e6d9dca5
Adam Zakaria <adam.p.zakaria@gmail.com>
2024-12-02 05:06:05 -0500

    Save 2_1

---
+++ 2_1.rb
@@ -0,0 +1,26 @@
+safe_count = 0
+File.open('2_1.txt', 'r') do |f|
+  f.each_line do |line|
+    ascending = descending = t2 = false
+    numbers = line.split.map(&:to_i) # Convert line to an array of integers
+
+    # Check if not ascending
+    ascending = numbers == numbers.sort
+
+    # Check if not descending
+    descending = numbers == numbers.sort.reverse
+
+    # Check if any adjacent element does not differ by AT LEAST 1 AND AT MOST 3
+    #t2 = numbers.each_cons(2).all? { |a, b| (a - b).abs.between?(1, 3) }
+    in_range = numbers.each_cons(2).all? { |a, b| (1 <= (a-b).abs) && ((a-b).abs <= 3) }
+
+    # Increment safe_count if any condition is true
+    safe_count += 1 if (ascending or descending) and in_range
+    ascending = descending = t2 = false
+    print numbers, "\n"
+    print "ascending #{ascending} descending #{descending} in_range #{in_range}\n"
+    print "*********************\n"
+  end
+end
+
+puts safe_count
